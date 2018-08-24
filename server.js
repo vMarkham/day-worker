bdec42cdedac8eca585d5a52184452a18dffe540
Justin <justhays124@gmail.com>
2018-08-24 11:35:34 -0700

    error handling imlpemented and formatted structure

--- server.js
+++ server.js
@@ -5,33 +5,29 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const path = require('path')
-
-const {
-    userRouter: users,
-    helpWantedRouter: helpWanted
-} = require('./routes')
+const { userRouter, helpWantedRouter } = require('./routes')
+const processErrorMessage = require('./middleware/errors')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
-app.use('/api/users', users )
-app.use('/api/helpWanted', helpWanted )
+app.use('/api/users', userRouter)
+app.use('/api/helpWanted', helpWantedRouter)
 
 
 app.use((req, res) => {
     const status = 404
     const message = `Could not ${req.method} ${req.path}`
     res.status(status).json({ status, message })
-});
+})
 
 app.use((err, _req, res, _next) => {
-    console.error(err)
+    err = processErrorMessage(err)
+    if (process.env.NODE_ENV !== 'test') console.error(err)
     const status = err.status || 500
     const message = err.message || 'Something went wrong!'
     res.status(status).json({ message, status })
 })
 
-app.listen(port, () => {
-    console.log('listening on port', port)
-})
+app.listen(port, () => console.log('listening on port', port))
