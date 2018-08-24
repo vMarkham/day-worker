48d38b32549c5cc9a9c978335987152988747bff
Justin <justhays124@gmail.com>
2018-08-24 11:38:22 -0700

    formatting and .then() chaining using model methods

--- ctrl/users.js
+++ ctrl/users.js
@@ -1,77 +1,44 @@
 const ctrl = require('./ctrl.js')(`users`)
 const { usersModel } = require('../model')
 
+
 class users extends ctrl{
   constructor(){
     super()
   } 
 
-  static checkUser(req, res, next){
-    const {
-      name, username, email, password, zipcode, phone
-    } = req.body.create
-
-    if (!name) throw new Error ('registerNameWrong')
-    if (!username) throw new Error ('registerUsernameWrong')
-    if (!password) throw new Error ('registerPasswordWrong')
-    if (!zipcode) throw new Error ('registerZipWrong')
-    if (!phone) throw new Error ('registerPhoneWrong')
-    if (!email) throw new Error ('registerEmailWrong')
-
-    // !name ? res.status(400).json({message: 'Please supply a name'}) : null
-    // !username ? res.status(400).json({message: 'Please supply a username'}) : null
-    // !password ? res.status(400).json({message: 'Please supply a password'}) : null
-    // !zipcode ? res.status(400).json({message: 'where are you looking for work?'}) : null
-    // !phone ? res.status(400).json({message: 'How do we contact you?'}) : null
-    // !email ? res.status(400).json({message: 'Please supply an Email'}) : null
-    // next()
-  }
-
-  static availableCred(req, res, next){
+  static isValidUserCreate (req, res, next) {
     const { name, username, email, password, zipcode, phone } = req.body.create
-    if (!name) throw new Error ('registerNameWrong')
-    if (!username) throw new Error ('registerUsernameWrong')
-    if (!password) throw new Error ('registerPasswordWrong')
-    if (!zipcode) throw new Error ('registerZipWrong')
-    if (!phone) throw new Error ('registerPhoneWrong')
-    if (!email) throw new Error ('registerEmailWrong')
-    usersModel.getUserByEmail(email)
-      .then(() => usersModel.getUserByUsername(username))
-      // .then(() => )
-      .catch(err => next(err))
-
-    // usersModel.all()
-      //   .then(result=>{
-      //     let collection = {}
-      //     result.forEach(user=>{
-      //       if (user.username == req.body.create.username){
-      //         collection.username = 'taken'
-      //         }
-      //       if(user.email == req.body.create.email){
-      //         collection.email='taken'
-      //       }
-      //     })
-      //     return collection
-      //   })
-      // .then(result=>{
-      //   console.log(result, 'after checks')
-      //   if(result.email==='taken'){
-      //     return res.status(400).json({message:'that email is taken'})
-      //   }
-      //   if(result.username==='taken'){
-      //     return res.status(400).json({message:'that username is taken'})
-      //   }
-      //   else{
-      //     console.log('Email and username not taken');
-      //     next()
-      //   }
-      // })
+    if (!name || typeof name !== 'string') throw new Error ('registerNameWrong')
+    if (!username || typeof username !== 'string') throw new Error ('registerUsernameWrong')
+    if (!password || typeof password !== 'string') throw new Error ('registerPasswordWrong')
+    if (!zipcode || typeof zipcode !== 'number') throw new Error ('registerZipWrong')
+    if (!phone || typeof phone !== 'string') throw new Error ('registerPhoneWrong')
+    if (!email || typeof email !== 'string') throw new Error ('registerEmailWrong')
+    next()
   }
 
+  static isValidUserPatch (req, res, next) {
+    const { name, username, email, password, zipcode, phone } = req.body.create
+    if (!name && !username && !email && !password && !zipcode && !phone) throw new Error('aFieldRequired')
+    next()
+  }
+
+  static availableCred(req, res, next) {
+    const { username, email } = req.body.create    
+    usersModel.getUserByEmail(email)
+      .then(user => {     
+        if (user) throw new Error('emailTaken')        
+        return usersModel.getUserByUsername(username)
+      })
+      .then(user => {    
+        if (user) throw new Error('usernameTaken')        
+        next()
+      })
+      .catch(err => next(err))
+  }
 
 }
-
-
 
 
 module.exports = users
