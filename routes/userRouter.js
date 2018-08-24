2afd020a4cd397fa4ac4f57a336c768eced22f4d
Justin <justhays124@gmail.com>
2018-08-24 10:07:43 -0700

    reworking for user routing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,19 @@
 const router = require('express').Router()
 const { users , tokenCtrl } = require('../ctrl')
 
+
 router.post('/login', tokenCtrl.checkPass, tokenCtrl.makeToken)
+
 router.get('/:id', users.one)
-// router.post('/new', users.checkUser, users.availableCred, users.create)
+
+// router.post('/register', users.checkUser, users.availableCred, users.create)
+
 router.post('/new', users.availableCred, users.create)
 
-
-
 router.patch('/:id', users.update)
+
 router.delete('/:id', users.destroy)
 
 
 module.exports = router
+ 
