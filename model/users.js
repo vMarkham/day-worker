66d7450dac9f68691eded440a2d24a22ef4ea7ec
Vincent Markham <vincent.markham@gmail.com>
2018-09-04 20:03:55 -0700

    bugfix for isow and skills migrations, add user-worker profile join query

--- model/users.js
+++ model/users.js
@@ -17,13 +17,38 @@ class usersModel extends Model {
     return db('users')
       .where({ email })
       .first()
-  } 
+  }
 
   static getUserByUsername(username) {
     return db('users')
       .where({ username })
       .first()
   }
+
+  static userWithWorkerProfile(userId){
+    return db('users')
+    .select([
+      'users.id',
+      'users.name',
+      'users.username',
+      'users.email',
+      'users.zipcode',
+      'users.phone',
+      // 'users.user_type',
+      'worker.id as workerID',
+      'worker.availability',
+      'worker.total_skills',
+      'worker.total_projects as totalProjects',
+      'worker.blurb',
+      'worker.work_zips as workZips',
+      'worker.about_me_paragraph',
+      'worker.about_me_interests'
+    ])
+    .leftJoin('worker_profiles as worker', 'worker.user_ID', 'users.id')
+    .where('users.id', '=', userId)
+  }
+
+
 
 }
 
