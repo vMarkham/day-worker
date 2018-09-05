f704bdadf4c2687d4ee4cf9680f9c418c1a6ca21
Vincent Markham <vincent.markham@gmail.com>
2018-09-04 20:31:42 -0700

    Add pseudo user profile routes, commented out for now.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -13,5 +13,18 @@ router.patch('/:id', users.update)
 router.delete('/:id', users.destroy)
 
 
+//Proposed user worker profile routes:
+// router.get('/profile/worker/:id', users.oneWorkerProfile)
+
+// router.post("/profile/worker/:id", users.isValidWorkerProfileCreate, users.createWorkerProfile)
+
+// router.patch('/profile/worker/:id', users.updateWorkerProfile)
+
+//Proposed user reviewer profile routes:
+// router.get('/profile/reviewer/:id', users.oneReviewerProfile)
+
+// router.post("/profile/reviewer/:id", users.isValidReviewerProfileCreate, users.createReviewerProfile)
+
+// router.patch('/profile/reviewer/:id', users.updateReviewerProfile)
+
 module.exports = router
- 
